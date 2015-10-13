8266fbb3b54e4b55fd56a4886b5af6c3bd5615ed
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-12 22:17:33 -0400

    wagon-git commit

---
+++ mybatis-generator/mybatis-generator-systests-common/apidocs/reference/lists.js
@@ -0,0 +1,11 @@
+var DATA = [
+      { id:0, label:"mbg.test.common", link:"mbg/test/common/package-summary.html", type:"package" },
+      { id:1, label:"mbg.test.common.BaseClass", link:"mbg/test/common/BaseClass.html", type:"class" },
+      { id:2, label:"mbg.test.common.BaseInterface", link:"mbg/test/common/BaseInterface.html", type:"class" },
+      { id:3, label:"mbg.test.common.FirstName", link:"mbg/test/common/FirstName.html", type:"class" },
+      { id:4, label:"mbg.test.common.MyTime", link:"mbg/test/common/MyTime.html", type:"class" },
+      { id:5, label:"mbg.test.common.util", link:"mbg/test/common/util/package-summary.html", type:"package" },
+      { id:6, label:"mbg.test.common.util.SqlScriptRunner", link:"mbg/test/common/util/SqlScriptRunner.html", type:"class" },
+      { id:7, label:"mbg.test.common.util.TestUtilities", link:"mbg/test/common/util/TestUtilities.html", type:"class" }
+
+    ];
