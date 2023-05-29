e3c34cb43132910ba929b9be890d93502c6b7e54
Jeremy Landis <jeremylandis@hotmail.com>
2023-05-29 19:28:45 -0400

    [cleanup] Remove misplaced mybatis-generator site

--- mybatis-generator/mybatis-generator-maven-plugin/apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.mybatis.generator.maven", "org/mybatis/generator/maven/package-summary.html", [ [ "Classes", null, [ [ "HelpMojo", "org/mybatis/generator/maven/HelpMojo.html", null, "" ], [ "MavenLogFactory", "org/mybatis/generator/maven/MavenLogFactory.html", null, "" ], [ "MavenLogImpl", "org/mybatis/generator/maven/MavenLogImpl.html", null, "" ], [ "MavenProgressCallback", "org/mybatis/generator/maven/MavenProgressCallback.html", null, "" ], [ "MavenShellCallback", "org/mybatis/generator/maven/MavenShellCallback.html", null, "" ], [ "MyBatisGeneratorMojo", "org/mybatis/generator/maven/MyBatisGeneratorMojo.html", null, "" ], [ "SqlScriptRunner", "org/mybatis/generator/maven/SqlScriptRunner.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
